0e69b1ecd0c7d4f85c43cd5120798e1e4052cb36
Ian Tonkikh <Ian_Tonkikh@epam.com>
2018-07-24 12:48:39 +0300

    hipster

--- bot.js
+++ bot.js
@@ -8,7 +8,11 @@ const keyboard = Markup.inlineKeyboard([
 ])
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Hello'))
+bot.start((ctx) => {
+    ctx.reply('Добро пожаловать на Поле Чудес!');
+});
+
+bot.command('hipster', Telegraf.reply('λ'))
 bot.help((ctx) => ctx.reply('Help message'))
 bot.on('message', (ctx) => ctx.telegram.sendCopy(ctx.from.id, ctx.message, Extra.markup(keyboard)))
 bot.action('delete', ({ deleteMessage }) => deleteMessage())
